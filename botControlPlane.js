a1db8bd22a3b27ce5ded57f5710896a5ecbc3bf4
Tennsoku <olvantorxu@gmail.com>
2020-06-07 03:08:44 -0400

    botCP handler updated. Thumbup available. Working on kudoAdmin logic.

--- botControlPlane.js
+++ botControlPlane.js
@@ -3,21 +3,31 @@ const botconfig = require("./botconfig.json");
 const kudoDescData = require("./KudoDescDataInstance.js"); //kudo desc
 //const kudoAdminData = require("./FileUtilSingleton.js"); //kudo desc
 //const kudoPtData = require("./FileUtilSingleton.js"); //kudo desc
-
+const guildID = '719042359651729418'; // TODO: currently hard coded for test server. 
 
 const client = new discord.Client({disableEveryone: true});
 
 client.on("ready", async() => {
 	client.user.setActivity("劫，剑姬，刀妹");
+	// console.log(client.guilds);
+	console.log(client.guilds.cache.keys().next().value); // Get guild ID here
+	// kudoAdminData.setGuild(client.guilds.cache.get(guildID));
+	
+
 });
 
 client.on("message", async message => {
 	//console.log(message);
 	console.log(
-		"\nMessage received from server: " + message.guild.name +
-		"\nSender: " + message.author.username +
+		"\nMessage received from server: \033[1;34m" + message.guild.name + "\033[0m" +
+		"\nSender: \033[1;31m" + message.author.username + "\033[0m" +
 		"\nContent: \"" + message.content + "\""
 	)
+
+	if(!message.guild.available) {
+		console.log("Handler: Server unavailable. Ignore.");
+		return;
+	}
 
 	if(message.author.bot) {
 		console.log("Handler: Bot message received. Ignore.");
@@ -30,12 +40,33 @@ client.on("message", async message => {
 	let messageArray = message.content.split(" ");
 	let cmd = messageArray[0];
 	//let args = messageArray.slice(1);
-	if(cmd === `${prefix}kudoToken`)
-		return message.channel.send(handleKudoTokenReturn(messageArray));
-	//else if (cmd === `${prefix}thumbupTest`)
-		//console.log(discord.MessageReaction(client, null, message));
-	
-	return message.channel.send(handleShadiaoReturn(cmd));
+
+	if(cmd[0]!==prefix) {
+		console.log("Handler: Not a vaild command. Ignore.");
+		return;
+	}
+
+	switch (cmd) {
+		case `${prefix}kudoPt`:
+			return message.channel.send(handleKudoPtReturn(messageArray));
+			break;
+		
+		case `${prefix}thumbupTest`:
+			return message.react('👍');
+			break;
+
+		case `${prefix}help`:
+			return message.channel.send(
+				`
+Current Available:
+/kudoPt --get <Player Name> --endorse <Player Name>
+/thumbupTest
+				`);
+			break;
+		
+		default:
+			return message.channel.send("Undefinded action. Try /help for more available options.");
+	}
 });
 
 function handleShadiaoReturn(inputMessage){
@@ -51,9 +82,9 @@ function handleShadiaoReturn(inputMessage){
 	}else return "Undefined Action.";
 }
 
-function handleKudoTokenReturn(inputMessage) {
+function handleKudoPtReturn(inputMessage) {
 	if(!inputMessage[1])
-		return "Not valid command, do you mean: \n/kudoToken get <Player Name>\n" +
+		return "Not valid command, do you mean: \n/kudoPt get <Player Name>\n" +
 		"/kudoToken endorse <Player Name>";
 	
 	if(inputMessage[1] === "get"){
@@ -67,8 +98,8 @@ function handleKudoTokenReturn(inputMessage) {
 		var a = kudoDescData.getPlayerToken(inputMessage[2]) + 1;
 		return kudoDescData.setPlayerToken(inputMessage[2], a);
 	}else{
-		return "Not valid command, do you mean: \n/kudoToken get <Player Name>\n" +
-		"/kudoToken endorse <Player Name>";
+		return "Not valid command, do you mean: \n/kudoPt get <Player Name>\n" +
+		"/kudoPt endorse <Player Name>";
 	}
 }
 
