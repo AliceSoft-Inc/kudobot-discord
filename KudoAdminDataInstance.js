53a4c2dbe40fab4b4edd4eaa2dc88383d0a27db0
Tennsoku <olvantorxu@gmail.com>
2020-06-10 08:34:18 +0900

    Create KudoAdminDataInstace.js

---
+++ KudoAdminDataInstance.js
@@ -0,0 +1,51 @@
+const DataUtil = require("./DataUtil.js");
+const fileName = './database/kudoAdmin.json';
+const fileEncoding = 'utf8';
+
+var instance = (function() {
+	let dataUtil = new DataUtil(fileName, fileEncoding);
+	var data = dataUtil.read();
+  
+	function getData() { //test use
+		return data;
+	}
+	
+	function assignAdmin(userID) { 
+		if (verifyuserID(userID))
+			return "error: Admin " + userID + " already existed.";
+
+		data[userID] = true;
+		dataUtil.write(data);
+		return `Successfully assigned userID: ${userID} as admin.`; //TODO: exception handle
+	}
+
+	function isAdmin(userID) { 
+		return verifyuserID(userID); //TODO: exception handle
+	}
+
+	function rmAdmin(userID) {
+		if (!verifyuserID(userID))
+			return "error: User" + userID + " is not under admin premission.";
+
+		data[userID] = false;
+		dataUtil.write(data);
+		return `Successfully removed userID: ${userID} from admin.`; //TODO: exception handle
+	}
+
+	function verifyuserID(userID) {
+		if (!data[userID])
+			return false;
+
+		return true;
+	}
+	
+	
+  return { // public interface
+	getData: getData,
+	assignAdmin: assignAdmin,
+	rmAdmin: rmAdmin,
+	isAdmin: isAdmin
+  };
+})();
+
+module.exports = instance
